5e266ec1cc56fe1162905a385b800c59ff8fa6a5
original taste <originaltaste05@gmail.com>
2023-12-02 11:23:33 +0530

    modified config files

--- site.config.js
+++ site.config.js
@@ -1,25 +1,25 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "Surja",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "frontend developer",
     bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
+    email: "originaltaste05@gmail.com",
+    linkedin: "",
+    github: "original-taste",
     instagram: "",
   },
   projects: [
     {
-      name: `morethan-log`,
-      href: "https://github.com/morethanmin/morethan-log",
+      name: `originaltaste-tech-log`,
+      href: "https://github.com/original-taste/morethan-log",
     },
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
+    title: "originaltaste-tech-log",
+    description: "welcome to originaltaste-tech-log!",
   },
 
   // CONFIG configration (required)
